2074b6f114929fe99b928adb89431e8d46d0b751
nogju <nogju@pidmail.com>
2017-06-14 16:23:51 +0200

    Refactor block tests.

--- src/block.js
+++ src/block.js
@@ -1,12 +1,12 @@
 //block structure
 const sha = require('sha256')
 
-function addTransaction(hash,transaction){
+function addTransaction(hash, transaction){
   hash.data.push(transaction);
   return hash;
 }
 
-function computeHash(block,nonce){
+function computeHash(block, nonce){
   return sha.x2(nonce + block.data + block.hash);
 }
 
@@ -21,6 +21,7 @@ function printBlock(block){
   console.log("previous hash : " + block.hash);
   console.log("data : " + block.data);
 }
+
 module.exports = {
   block : block,
   computeHash : computeHash,
--- test/block.js
+++ test/block.js
@@ -1,31 +1,27 @@
 var assert = require('assert');
 let block = require('../src/block')
 
-describe('block', () => {
-    describe('#test1()', () => {
+describe('block.js', () => {
+    describe('#block()', () => {
         it("adds previous hash to block", () => {
             let b = block.block("hash is added");
             assert.equal(b.hash,"hash is added");
         });
     });
-});
 
-describe('block', () => {
-    describe('#test2()', () => {
+    describe('#addTransaction()', () => {
         it("adds new transaction", () => {
             let b = block.block("hash is added");
-            b = block.addTransaction(b, "first transaction");
-            assert.equal(b.data,"first transaction");
-        });
-    });
-});
+            block.addTransaction(b, "first transaction");
 
-describe('block', () => {
-    describe('#test3()', () => {
+            assert.deepEqual(b.data, ["first transaction"]);
+        });
+
         it("computes hash with nonce", () => {
             let b = block.block("hash is added");
-            b = block.addTransaction(b, "first transaction");
-            assert.equal(block.computeHash("hello",b).length,64);
+            block.addTransaction(b, "first transaction");
+
+            assert.equal(block.computeHash("hello",b).length, 64);
         });
     });
 });
